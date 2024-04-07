eef1bb77b832232716042ee782e276c64534a7a0
Lee Soo Kyung <55040739+Sookyung87@users.noreply.github.com>
2024-04-07 17:49:57 +0900

    test

---
+++ app/src/main/java/com/example/mobileprogramming_test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //테스트 중
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
