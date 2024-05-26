038269ae4b86c5077d652c7de673a016116189ee
Administrator <yj030917@naver.com>
2024-05-27 01:14:54 +0900

    수정중

--- app/src/main/java/com/example/mobileprogrammingjava/MainActivity.java
+++ app/src/main/java/com/example/mobileprogrammingjava/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.core.view.OnApplyWindowInsetsListener;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
